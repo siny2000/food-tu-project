f8eecd41af1976b97b8cd8fd9a3d5579c120f04c
Tanabut <tangg6@outlook.com>
2021-03-01 19:57:06 +0700

    del button store and user on home website

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="main">
     <h1>{{ msg }}</h1>
-<<<<<<< HEAD
 
     <!-- เลือก login หรือ register -->
     <div><router-link to="/Login">Login</router-link></div>
     <br>
     <div><router-link to="/Register">Register</router-link></div>
-    <!--ให้เลือกStore or User-->
-    <br>
-    <br>
-    <div><router-link to="/Store">Store</router-link></div>
-    <br>
-    <div><router-link to="/User">User</router-link></div>
     
     <!-- ปล. มันยังเป็นแค่ลิ้งค์ ถ้าว่างก็หาวิธีใส่ปุ่มให้หน่อยนะ -->
 
-=======
     <h1>Test by Ip</h1>
->>>>>>> 614eee5b3d4d042f74e541ec99442568ecd3d524
   </div>
 </template>
 

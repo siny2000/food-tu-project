bf645dac0ced9c9e490262260f1076937322ee3f
wuppsn <wuppsn@gmail.com>
2021-03-01 18:23:39 +0700

    T-T

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,11 +3,13 @@
   <div class="main">
     <h1>{{ msg }}</h1>
 
-    <!--ให้เลือกStore or User-->
+    <!--แถบข้างบนค้าบ-->
     <div class="topnav">
-      <a class="active" id="choose"> Please Select</a>
-      <router-link to="/Store">For Store</router-link>
-      <router-link to="/User">For User</router-link>
+      <router-link to="/Store">STORE</router-link>
+      <router-link to="/User">USER</router-link>
+      <router-link to="/About">ABOUT</router-link>
+      <router-link to="/Team">TEAM</router-link>
+      <router-link to="/FollowUs">FOLLOW US</router-link>
     </div>
   
     
@@ -20,7 +22,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome To TU food '
+      msg: ' '
     }
   }
 }
@@ -29,27 +31,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-button {
-  width: 50%;
-  height: 30px;
-  background-color:rgba(135, 88, 180, 0.425);
-}
-h1, h2 {
-  font-weight: normal;
-}
+
 
 .topnav {
+  height: 50px;
   overflow: hidden;
   background-color: #333;
 }
 
 .topnav a {
-  float: left;
+  
   color: #f2f2f2;
   text-align: center;
   text-decoration: none;
   font-size: 17px;
-  padding: 20px 60px;
+  padding: 40px 60px;
 }
 
 .topnav a:hover {
---
+++ src/components/Team.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="Team">
+        <!--แถบข้างบนค้าบ-->
+        <div class="topnav">
+            <router-link to="/Store">STORE</router-link>
+            <router-link to="/User">USER</router-link>
+            <router-link to="/About">ABOUT</router-link>
+            <router-link to="/Team">TEAM</router-link>
+            <router-link to="/FollowUs">FOLLOW US</router-link>
+        </div>
+
+    </div>
+</template>

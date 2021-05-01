b8a32f1d4199f273ed82c9e9563b668042fc1969
wuppsn <wuppsn@gmail.com>
2021-05-02 02:02:16 +0700

    add about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,28 @@
 <template>
+<body>
     <div class="About">
-    <h1>Test</h1>
+    <h1><strong> ABOUT </strong></h1>
+      <center><img src="../assets/โรงอาหาร.jpg"></center>
+    <p>
+      <strong> FOUNDED IN 2021 </strong>
+    </p>
+    <p>
+      <strong> BECAME THE FIRST TU FOOD APPLICATION. </strong>
+    </p>
+    <p>
+      ช่วยกุคิดหน่อย
+    </p>
+    <p>
+      ที่ตรงนี้จะเขียนเป็นอิ้งหรือไทยดี
+    </p>
+    <p>
+      3บรรทัดพอ
+    </p>
+
+
+
     </div>
+</body>
 </template>
 
 <script>
@@ -9,3 +30,26 @@ export default {
   name: 'About'
 }
 </script>
+<style scoped>
+h1 {
+  margin: 60px;
+  font-size: xx-large;
+
+}
+
+h2 {
+  font-size: larger;
+  margin: 80px;
+}
+p {
+  display: block;
+  margin: 60px;
+  align-content: flex-start;
+
+}
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 100%;
+  
+}
+</style>
